d1e6c3ec8defc72575bd408931d0400423132314
arunprakash12345 <arunprakash74119@gmail.com>
2026-03-03 09:23:26 +0530

    Changes done.

--- 14 day challenge/Arrays/LC152.java
+++ 14 day challenge/Arrays/LC152.java
@@ -2,7 +2,6 @@
 // The test cases are generated so that the answer will fit in a 32-bit integer.
 // Note that the product of an array with a single element is the value of that element.
 // Example 1:
-
 // Input: nums = [2,3,-2,4]
 // Output: 6
 // Explanation: [2,3] has the largest product 6.
