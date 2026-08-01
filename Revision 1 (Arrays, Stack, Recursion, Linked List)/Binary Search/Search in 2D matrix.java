cd9f030b4bf1b4b315dc4d711d36d419e2843cb8
arunprakash12345 <arunprakash74119@gmail.com>
2026-08-01 09:06:06 +0530

    Add binary search implementations for finding the median of two sorted arrays and searching a 2D matrix

---
+++ Revision 1 (Arrays, Stack, Recursion, Linked List)/Binary Search/Search in 2D matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int low = 0;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int high = (n * m) - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int i = mid / m;
+            int j = mid % m;
+            if (matrix[i][j] == target)
+                return true;
+            if (matrix[i][j] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return false;
+    }
+}
